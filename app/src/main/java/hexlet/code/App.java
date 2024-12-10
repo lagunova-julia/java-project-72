1de1b65e1ff28f0c38d19dd8a8890222165c3004
Julia <lagunaajul@gmail.com>
2024-12-11 01:22:44 +0500

    add App and Dockerfile

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,22 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
+public class App {
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            //config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
